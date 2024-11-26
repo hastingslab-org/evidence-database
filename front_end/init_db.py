b0db3df91178d651dff60bb4d8409e9461a696dd
niedox <thibault.niederhauser@gmail.com>
2024-11-26 18:40:31 +0100

    put files in front_end folder

---
+++ front_end/init_db.py
@@ -0,0 +1,11 @@
+import sqlite3
+
+connection = sqlite3.connect('database.db')
+
+with open('queries.sql') as f:
+    connection.executescript(f.read())
+
+cur = connection.cursor()
+
+connection.commit()
+connection.close()
