81cef3607f5fa35a0d3c1aceb67945674dcf7eca
niedox <thibault.niederhauser@gmail.com>
2024-11-26 18:27:21 +0100

    basic front end with search and answer pages

--- init_db.py
+++ init_db.py
@@ -2,19 +2,10 @@ import sqlite3
 
 connection = sqlite3.connect('database.db')
 
-
 with open('queries.sql') as f:
     connection.executescript(f.read())
 
 cur = connection.cursor()
-
-cur.execute("INSERT INTO queries (content) VALUES (?)",
-            ('Content for the first query',)
-            )
-
-cur.execute("INSERT INTO queries (content) VALUES (?)",
-            ('Content for the second query',)
-            )
 
 connection.commit()
 connection.close()
