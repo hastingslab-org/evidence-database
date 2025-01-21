82d2b8fe7769ab5c438dc0bd73b1021c7b4f17c3
niedox <thibault.niederhauser@gmail.com>
2025-01-21 16:23:36 +0100

    response in db

--- init_db.py
+++ init_db.py
@@ -6,6 +6,12 @@ with open('queries.sql') as f:
     connection.executescript(f.read())
 
 cur = connection.cursor()
-
+cur.execute('''
+        CREATE TABLE IF NOT EXISTS llm_responses (
+            id TEXT PRIMARY KEY,
+            query TEXT,
+            response TEXT
+        )
+    ''')
 connection.commit()
 connection.close()
