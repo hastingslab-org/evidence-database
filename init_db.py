27e540c64c48b9a8f111d4f3d3b559198d28d6e3
niedox <thibault.niederhauser@gmail.com>
2025-01-27 11:09:29 +0100

    fix response and qa data storage

--- init_db.py
+++ init_db.py
@@ -2,14 +2,16 @@ import sqlite3
 
 connection = sqlite3.connect('database.db')
 
-with open('queries.sql') as f:
+with open('responses.sql') as f:
     connection.executescript(f.read())
 
 cur = connection.cursor()
 cur.execute('''
-        CREATE TABLE IF NOT EXISTS llm_responses (
+        CREATE TABLE IF NOT EXISTS qa_data (
             id TEXT PRIMARY KEY,
             query TEXT,
+            patient_data TEXT,
+            papers TEXT,
             response TEXT
         )
     ''')
