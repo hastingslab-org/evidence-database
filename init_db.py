df13e1fecebdcbb9dc510fdeaad8126ef8c639a3
niedox <thibault.niederhauser@gmail.com>
2025-01-27 23:33:26 +0100

    some code cleaning

--- init_db.py
+++ init_db.py
@@ -1,19 +1,21 @@
 import sqlite3
 
-connection = sqlite3.connect('database.db')
+def init_db():
+    connection = sqlite3.connect('database.db')
 
-with open('responses.sql') as f:
-    connection.executescript(f.read())
+    with open('responses.sql') as f:
+        connection.executescript(f.read())
 
-cur = connection.cursor()
-cur.execute('''
-        CREATE TABLE IF NOT EXISTS qa_data (
-            id TEXT PRIMARY KEY,
-            query TEXT,
-            patient_data TEXT,
-            papers TEXT,
-            response TEXT
-        )
-    ''')
-connection.commit()
-connection.close()
+    cur = connection.cursor()
+    cur.execute('''
+            CREATE TABLE IF NOT EXISTS qa_data (
+                id TEXT PRIMARY KEY,
+                query TEXT,
+                patient_data TEXT,
+                papers TEXT,
+                response TEXT
+            )
+        ''')
+
+    connection.commit()
+    connection.close()
