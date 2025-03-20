93e19b9a2c5d15323ab35e7e7a79fa38bb449abc
niedox <thibault.niederhauser@gmail.com>
2025-03-20 17:29:37 +0100

    gene SQL table

--- genomics.sql
+++ genomics.sql
@@ -8,3 +8,14 @@ CREATE TABLE IF NOT EXISTS molecular_profiles (
     molecularProfileScore REAL,
     db_source TEXT
 )
+
+DROP TABLE IF EXISTS genes;
+CREATE TABLE IF NOT EXISTS genes (
+    id TEXT PRIMARY KEY,
+    name TEXT,
+    description TEXT,
+    variants JSON, -- Store list of variant IDs as a JSON array
+    molecular_profiles JSON, -- Store list of molecular profile IDs as a JSON array
+    disease JSON,  -- Store list of disease IDs as a JSON array
+    db_source TEXT
+);
