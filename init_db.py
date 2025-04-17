a72064c0eedd6e3b74d66147c3bf4bfa3d8494ba
niedox <thibault.niederhauser@gmail.com>
2025-04-17 18:51:02 +0200

    retrieve from info correctly

--- init_db.py
+++ init_db.py
@@ -9,13 +9,5 @@ def init_db():
     with open('responses.sql') as f:
         connection.executescript(f.read())
 
-    global G, df_consensus #TODO are gloobal variables the best approach? Check session variables
-    # Load datasets needed for network query (variantscape)
-    print("Loading the network graph...")
-    G = nx.read_gml("network_graph_weighted.gml")
-    print("Loading the consensus file...")
-    df_consensus = pd.read_csv('final_variant_treatment_consensus.csv')
-
-
 if __name__ == "__main__":
     init_db()
